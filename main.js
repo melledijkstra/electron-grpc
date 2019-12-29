a16b4d141770e72052f689acaa0701561afa022f
Melle Dijkstra <melle210202@gmail.com>
2019-12-29 21:31:14 +0100

    gRPC code

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const {app, BrowserWindow} = require('electron')
+const service = require('./proto/chat_grpc_pb')
+const chat = require('./proto/chat_pb')
+const grpc = require('grpc')
 
 function createWindow() {
     // Create the browser window.
@@ -10,8 +13,21 @@ function createWindow() {
         }
     })
 
+    win.removeMenu();
+    // Open the DevTools.
+    win.webContents.openDevTools({
+        mode: "detach"
+    })
     // and load the index.html of the app.
     win.loadFile('index.html')
+
+    let client = new service.ChatServerClient('localhost:11912', grpc.credentials.createInsecure())
+    let note = new chat.Note()
+    note.setMessage('Hello from electron')
+    console.log('sending note to server')
+    client.sendNote(note, function(err, response) {
+        console.log('received reply', response);
+    });
 }
 
 app.on('ready', createWindow)
